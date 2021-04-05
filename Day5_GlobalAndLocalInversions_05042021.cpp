2eef943ce7eb115e19fd0bef278aaa1487aa1e77
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-04-05 18:59:37 +0700

    max(max, A[i)) > A[i+2} to ensure that no exceed global inversions

---
+++ Day5_GlobalAndLocalInversions_05042021.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool isIdealPermutation(vector<int> &A) {
+    int ma = -1;
+    for(int i = 0; i < A.size() - 2; ++i){
+        ma = max(ma, A[i]);
+        if(ma > A[i+2]) return false;
+    }
+    return true;
+}
+
+int main(){
+	vector<int> A;
+	A.push_back(1);
+	A.push_back(0);
+	A.push_back(2);
+	A.push_back(4);
+	A.push_back(3);
+	cout << isIdealPermutation(A);
+	return 0;
+}
