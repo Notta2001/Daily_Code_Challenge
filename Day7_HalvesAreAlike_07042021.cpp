41e2074cc700e8300a2f628ac8ec7e4924797a3d
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-04-08 00:07:43 +0700

    check if two halves have the same number of vowels, use 2 parameters in one for loop

---
+++ Day7_HalvesAreAlike_07042021.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool halvesAreaAlike(string s){
+	int res = 0;
+	char a[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+	vector<char>symb (a, a + sizeof(a)/sizeof(char));
+	for(int i = 0, k = s.size()/2; k <s.size(); ++i, ++k){
+		if(find(symb.begin(), symb.end(), s[i]) != symb.end())
+			res ++;
+		if(find(symb.begin(), symb.end(),s[k]) != symb.end())
+                res--;
+    }
+	return res == 0;
+}
+
+int main(){
+	string s = "MerryChristmas";
+	cout << halvesAreaAlike(s);
+	s = "book";
+	cout << halvesAreaAlike(s);
+	return 0;
+}
