f9aec00e8699a66367bbd56e4b21d44d2c95be64
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-04-10 00:52:13 +0700

    Check wether the list of string is sorted in an alien language

---
+++ Day9_VerifyingAnAlienDictionary_09042021.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> dict(26,0);
+
+bool bigger (const string &s1, const string &s2){
+	const int length1 = s1.length();
+	const int length2 = s2.length();
+	for(int i = 0; i < min(length1, length2); ++i){
+		if(dict[s1[i] - 'a'] < dict[s2[i] - 'a']) return false;
+		if(dict[s1[i] - 'a'] > dict[s2[i] - 'a']) return true;
+	}
+	return length1 > length2;
+}
+
+bool isAlienSorted(vector<string> &words, string order){
+	for(int i = 0; i < 26; ++i){
+		dict[order[i] - 'a'] = i;
+	}
+	for(int i = 0; i < words.size()-1; ++i){
+		if(bigger(words[i], words[i+1])){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(){
+	vector<string> str;
+	str.push_back("hello");
+	str.push_back("letcode");
+	string order = "hlabcdefgijkmnopqrstuvwxyz";
+	cout << isAlienSorted(str, order);
+	return 0;
+}
