5314bcda0dbdbd49ebc0f0c904129f05c8a9faf0
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-04-13 09:10:16 +0700

    Beautiful array with some trick

---
+++ Day11_BeautifulArrangementII_11042021.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> constructArray(int n, int k){
+	vector<int> res;
+	int i = 1, j = n;
+    while(i <= j)
+    {
+        if(k > 1) res.push_back(k-- % 2 ? i++ : j--);
+        else res.push_back(i++);
+            
+    }
+	for(int i = 0; i < res.size(); ++i){
+		cout << res[i] << " ";
+	}
+	return res;
+}
+
+int main(){
+	int n, k;
+	cin >> n >> k;
+	constructArray(n,k);
+	return 0;	
+}
