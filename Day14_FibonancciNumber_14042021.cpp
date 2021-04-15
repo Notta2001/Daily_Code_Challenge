26a5a13334065fdb741aa359629ecf6b1444ac6e
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-04-16 05:25:31 +0700

    Advanced version with recursive

---
+++ Day14_FibonancciNumber_14042021.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> memoize(31, -1);
+
+int fib(int n){
+	if(memoize[n] != -1) return memoize[n];
+	memoize[n] = fib(n-1) + fib(n-2);
+	return memoize[n];
+}
+
+int main(){
+	memoize[0] = 0;
+ 	memoize[1] = 1;
+	cout << fib(27);
+	return 0;
+}
