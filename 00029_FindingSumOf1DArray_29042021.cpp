e619818b313b3c706740ad45352ed01c5c8c9bce
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-05-03 15:48:51 +0700

    One loop with curSum variable

---
+++ 00029_FindingSumOf1DArray_29042021.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> runningSum(vector<int> & nums){
+	int curSum = 0;
+	vector<int> res;
+	for(int i = 0; i < nums.size(); ++i){
+		curSum += nums[i];
+		res.push_back(curSum);
+	}
+	return res;
+}
+
+void print(const vector<int> &nums){
+	for(int i = 0; i < nums.size(); ++i){
+		cout<< nums[i] << " " ;
+	}
+	cout << endl;
+}
+
+int main(){
+	int arr[5] = {1, 2, 3, 4, 5};
+	vector<int> init(&arr[0], &arr[0] + 5);
+	vector<int> res = runningSum(init);
+	print(init);
+	print(res);
+	return 0;
+}
