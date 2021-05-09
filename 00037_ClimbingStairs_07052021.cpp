c12a504b727ac56a2d41f40a9e98fb9919350f0c
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-05-09 19:34:40 +0700

    Caculate how many distinct ways can we climb to the nth stairs(we can climb just 1 or 2 steps per time. Using dynamic programming

---
+++ 00037_ClimbingStairs_07052021.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+long long int climbStairs(int n){
+	if(n == 1) return 1;
+	if(n == 2) return 2;
+	vector<long long int> res(n+1, 0);
+	res[1] = 1;
+	res[2] = 2;
+	for(int i = 3; i <= n; ++i){
+		res[i] = res[i-1] + res[i-2];
+	}
+	return res[n];
+}
+
+int main(){
+	cout << climbStairs(5) << endl;
+	cout << climbStairs(100) << endl;
+	cout << climbStairs(21) << endl;
+	cout << climbStairs(87) << endl;
+	return 1;
+}
