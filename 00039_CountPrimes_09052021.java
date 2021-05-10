a7aa53a8b520ccd4a893f939b8e97fbcdb8afeac
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-05-10 15:15:59 +0700

    smart way using array to count how many primes from 1 to n

---
+++ 00039_CountPrimes_09052021.java
@@ -0,0 +1,29 @@
+
+public class CountPrimes {
+	public static int countPrimes(int n) {
+		boolean[] isPrime = new boolean[n];
+		for(int i = 2; i < n; ++i) {
+			isPrime[i] = true;
+		}
+		// Loop's ending condition is i*i < n instead of i < sqrt(n)
+		// to avoid repeated calling an expensive function sqrt()
+		for(int i = 2; i * i < n; ++i) {
+			if(!isPrime[i]) continue;
+			for(int j = i * i; j < n; j += i) {
+				isPrime[j] = false;
+			}
+		}
+		int count = 0;
+		for(int i = 2; i < n; ++ i) {
+			if(isPrime[i]) count ++;
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		int n1 = 54643;
+		int n2 = 21;
+		System.out.println(countPrimes(n1));
+		System.out.println(countPrimes(n2));
+	}
+}
