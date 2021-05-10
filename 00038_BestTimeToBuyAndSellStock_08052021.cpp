bee24f621364835caa5e798dffedae1fa84039fb
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-05-10 09:35:05 +0700

    Find best time to buy and sell stock in the given array

---
+++ 00038_BestTimeToBuyAndSellStock_08052021.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int maxProfit(vector<int> &prices){
+	int res = 0;
+	int min_price = INT_MAX;
+	
+	for(int i = 0; i < prices.size(); ++i){
+		min_price = min(min_price, prices[i]);
+		res = max(res, prices[i] - min_price);
+	}
+	
+	return res;
+}
+
+int main(){
+	int stock1[6] = {7, 1, 5, 3 ,6 , 4};
+	int stock2[4] = {3, 4, 2, 6};
+	vector<int> init1(&stock1[0], &stock1[0]+6);
+	vector<int> init2(&stock2[0], &stock2[0]+4);
+	
+	cout << maxProfit(init1) << endl;
+	cout << maxProfit(init2) << endl;
+	
+	return 0;
+}
