c8bff860bc6b60b0e72fde28fa813707c6f06758
DESKTOP-CKFI5BR\LENOVO <doanthang2001@gmail.com>
2021-05-11 15:37:33 +0700

    Find the max total, note that you take only one from the beginning or from the end each step

---
+++ 00040_MaximumPointsYouCanObtainFromCard_10052021.java
@@ -0,0 +1,29 @@
+public class MaximumPointsYouCanObtainFromCard {
+	public static int maxScore(int[] cardPoints, int k) {
+		int max;
+		int res = 0;
+		int left = 0;
+		int right = k - 1;
+		
+		for(int i = 0; i < k; ++i) {
+			res += cardPoints[i];
+		}
+		
+		max = res; 
+		
+		while(right > 0) {
+			res = res - cardPoints[right--] + cardPoints[left-- + cardPoints.length - 1];
+			if(res > max) max = res;
+ 		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		int init1[] = {1, 2, 3, 4, 5, 6, 1};
+		int init2[] = {1, 79, 80, 1, 1, 1, 200, 1};
+		
+		System.out.println(maxScore(init1, 3));
+		System.out.println(maxScore(init2, 4));
+	}
+}
