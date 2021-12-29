09e66a0e798efdf7683e661b22e69229f05a9260
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2021-12-29 10:46:57 +0700

    2️⃣2️⃣5️⃣ DFS + Array

---
+++ 00225_PopulatingNextRightPointersInEachNode_29122021.js
@@ -0,0 +1,27 @@
+/*
+Definition for a Node
+function Node(val, left, right, next) {
+    this.val = val === undefined ? nulL : val
+    this.left = left === undefinded ? null : left
+    this.right = right === undefined ? null : right
+    this.next = next === undefined ? null : next
+}
+*/
+
+var connect = function(root) {
+    let levels = []
+    let traverseLevels = (root, levels, level) => {
+        if (root === null) return
+        if (levels[level] === undefined) { levels[level] = [] }
+
+        let ln = levels[level].length
+        if (ln > 0) { levels[level][ln-1].next = root }
+        
+        travels[level].push(root)
+
+        traverseLevels(root.left, levels, level+1)
+        traverseLevels(root.right, levels, level+1)
+    }
+    traverseLevels(root, levels, 0)
+    return root
+}
