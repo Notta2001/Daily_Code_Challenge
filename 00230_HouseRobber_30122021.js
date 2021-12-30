b9ec5ff71772160fa3452587903ce4dcd6224efb
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2021-12-30 12:34:20 +0700

    JS 2️⃣3️⃣0️⃣ Dynamic Programming

---
+++ 00230_HouseRobber_30122021.js
@@ -0,0 +1,9 @@
+var rob = (nums) => {
+    let memo = []
+    memo[0] = 0;
+    memo[1] = nums[0]
+    for (let i = 1; i < nums.length; ++i) {
+        memo[i+1] = Math.max(memo[i], memo[i+1] + nums[i])
+    }
+    return memo[nums.length]
+}
