ff430b1292e0c050ba03ff720fdb101c18fa5e16
ThangDoan2001 <doanthang2001@gmail.com>
2022-01-04 11:48:57 +0700

    0️⃣0️⃣2️⃣4️⃣9️⃣ JS Binary Search

---
+++ 00249_SearchInsertPosition_04012022.js
@@ -0,0 +1,13 @@
+var searchInsert = (nums, target) => {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = parseInt((right + left) / 2);
+        if (nums[mid] === target) 
+            return mid;
+        else if (nums[mid] < target) 
+            left = mid + 1;
+        else 
+            right = mid - 1;
+    }
+    return left;
+};
